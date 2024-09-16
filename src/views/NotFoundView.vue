293bdb796e054ed66e8c0e7bc3179d7f9872f7b2
oppen <arieloppenheimer@outlook.cl>
2024-09-16 20:34:20 -0300

    add:not found &&change:favicon.ico

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,30 @@
 <script setup>
+import router from '@/router'
+import { ref } from 'vue'
+const timer = ref(5)
+const interval = setInterval(() => {
+  if (timer.value <= 1) {
+    clearInterval(interval)
+    router.push('/')
+  }
+  timer.value--
+}, 1000)
 </script>
 <template>
-    <p>Not found</p>
+    <h1>Page not found</h1>
+    <h2>{{ timer }}</h2>
+    <router-link to="/" class="link">Ir a Home</router-link>
 </template>
 <style scoped>
+h1,h2{
+    text-align: center;
+}
+.link{
+    display: block;
+    width: fit-content;
+    margin: auto;
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
 </style>
