06cf536f056c2d55ae525dc314cd4f75bcdcf2af
oppen <arieloppenheimer@outlook.cl>
2024-09-16 21:35:15 -0300

    fix: collapse

--- src/components/CollapseComponent.vue
+++ src/components/CollapseComponent.vue
@@ -1,15 +1,17 @@
 <template>
-    <p class="d-inline-flex gap-1">
-  <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-    Link with href
-  </a>
-  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+  <div>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseName" aria-expanded="false" aria-controls="collapseName">
     Button with data-bs-target
   </button>
-</p>
-<div class="collapse" id="collapseExample">
+
+<div class="collapse border" id="collapseName">
   <div class="card card-body">
     Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
   </div>
+  <div>
+    <button class="btn btn-warning">Editar</button>
+    <button class="btn btn-danger">Eliminar</button>
+  </div>
+</div>
 </div>
 </template>
